af3d2f6034c4248340c9bd026a7f415e241310cd
Robert Loomans <robert@loomans.org>
2017-02-20 21:00:03 +1000

    Use word hash rather than word list to retrieve word cloud image

--- teamtemp/tests/view/test_wordcloud_view.py
+++ teamtemp/tests/view/test_wordcloud_view.py
@@ -1,12 +1,26 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from teamtemp.tests.factories import WordCloudImageFactory
+
 
 class WordcloudViewTestCases(TestCase):
-    def test_wordcloud_view(self):
-        response = self.client.get(reverse('wordcloud', kwargs={'word_list': ''}), follow=True)
-        self.assertRedirects(response, '/media/blank.png')
+    def assertWordCloudImage(self, response, url='/media/blank.png'):
+        self.assertRedirects(response, url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response['Content-Type'], 'image/png')
         self.assertEqual(response['Cache-Control'], 'public, max-age=315360000')
         self.assertGreater(len(response.getvalue()), 0)
+
+    def test_wordcloud_view_blank(self):
+        response = self.client.get(reverse('wordcloud', kwargs={'word_hash': ''}), follow=True)
+        self.assertWordCloudImage(response, '/media/blank.png')
+
+    def test_wordcloud_view_missing(self):
+        response = self.client.get(reverse('wordcloud', kwargs={'word_hash': '0123456789abcdef0123456789abcdef01234567'}), follow=True)
+        self.assertWordCloudImage(response, '/media/blank.png')
+
+    def test_wordcloud_view_found(self):
+        word_cloud_image = WordCloudImageFactory(image_url='/media/blank.png')
+        response = self.client.get(reverse('wordcloud', kwargs={'word_hash': word_cloud_image.word_hash}), follow=True)
+        self.assertWordCloudImage(response, word_cloud_image.image_url)
