26e3ca400cee2936c5990c26b3c70426dbdcd20a
Robert Loomans <robert@loomans.org>
2021-07-23 10:43:39 +1000

    ignore CSP reports that are for browser extensions

--- teamtemp/settings/base.py
+++ teamtemp/settings/base.py
@@ -210,6 +210,7 @@ CSP_IMG_SRC = ("'self'", 'data:', 'blob:',)
 CSP_FONT_SRC = ("'self'", 'data:', 'stackpath.bootstrapcdn.com', 'fonts.gstatic.com',)
 CSP_EXCLUDE_URL_PREFIXES = ("/djadmin",)
 CSP_REPORT_URI = reverse_lazy('report_csp')
+CSP_REPORTS_FILTER_FUNCTION = 'teamtemp.utils.filter_csp_report'
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
