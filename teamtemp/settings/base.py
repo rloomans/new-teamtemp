52233a6ad1105562deb7c034126f9b1937063f6b
Robert Loomans <robert@loomans.org>
2022-01-18 07:45:13 +1000

    fix some CSP rules

--- teamtemp/settings/base.py
+++ teamtemp/settings/base.py
@@ -206,8 +206,8 @@ CSP_STYLE_SRC = (
     'stackpath.bootstrapcdn.com',
     'www.gstatic.com',
 )
-CSP_IMG_SRC = ("'self'", 'data:', 'blob:',)
-CSP_FONT_SRC = ("'self'", 'data:', 'stackpath.bootstrapcdn.com', 'fonts.gstatic.com',)
+CSP_IMG_SRC = ("'self'", 'data:', 'blob:', 'www.gstatic.com',)
+CSP_FONT_SRC = ("'self'", 'data:', 'stackpath.bootstrapcdn.com', 'fonts.gstatic.com', 'fonts.googleapis.com',)
 CSP_EXCLUDE_URL_PREFIXES = ("/djadmin",)
 CSP_REPORT_URI = reverse_lazy('report_csp')
 CSP_REPORTS_FILTER_FUNCTION = "cspreports.filters.filter_browser_extensions"
@@ -234,7 +234,6 @@ GOOGLE_ANALYTICS_PROPERTY_ID = os.environ.get(
 GOOGLE_ANALYTICS_DOMAIN = os.environ.get('GOOGLE_ANALYTICS_DOMAIN', 'auto')
 
 if GOOGLE_ANALYTICS_PROPERTY_ID:
-    CSP_SCRIPT_SRC += ("'unsafe-eval'", "'unsafe-inline'",
-                       'www.google-analytics.com', 'data:',)
+    CSP_SCRIPT_SRC += ("'unsafe-eval'", "'unsafe-inline'", 'www.google-analytics.com', 'data:', 'ssl.google-analytics.com',)
     CSP_IMG_SRC += ('www.google-analytics.com',)
     CSP_CONNECT_SRC += ('www.google-analytics.com',)
