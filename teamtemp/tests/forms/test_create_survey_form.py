2d2074234ac25b7516e375712110957629ef9aa5
Robert Loomans <robert@loomans.org>
2022-06-22 12:55:06 +1000

    fix a broken test

--- teamtemp/tests/forms/test_create_survey_form.py
+++ teamtemp/tests/forms/test_create_survey_form.py
@@ -1,24 +1,31 @@
 from django.test import TestCase
 
 from teamtemp.tests.factories import TeamTemperatureFactory
-from teamtemp.responses.forms import SurveySettingsForm
+from teamtemp.responses.forms import CreateSurveyForm
 
 
-class SurveySettingsFormTestCases(TestCase):
+class CreateSurveyFormTestCases(TestCase):
+    def setUp(self):
+        self.survey = TeamTemperatureFactory()
+        self.form_data = {
+            'id': self.survey.id,
+            'new_password': self.survey.password,
+            'confirm_password': self.survey.password,
+            'dept_names': self.survey.dept_names,
+            'region_names': self.survey.region_names,
+            'site_names': self.survey.site_names,
+        }
+
     def test_empty_survey_settings_form(self):
-        form = SurveySettingsForm()
+        form = CreateSurveyForm()
         self.assertFalse(form.is_valid())
 
     def test_existing_survey_settings_form(self):
-        survey = TeamTemperatureFactory()
-        form_data = {
-            'id': survey.id,
-            'creator': survey.creator,
-            'password': survey.password,
-            'archive_schedule': survey.archive_schedule,
-            'survey_type': survey.survey_type,
-            'default_tz': survey.default_tz,
-            'max_word_count': survey.max_word_count
-        }
-        form = SurveySettingsForm(data=form_data)
+        form = CreateSurveyForm(data=self.form_data)
         self.assertTrue(form.is_valid())
+
+    def test_password_mismatch_survey_settings_form(self):
+        self.form_data["new_password"] = "one"
+        self.form_data["confirm_password"] = "two"
+        form = CreateSurveyForm(data=self.form_data)
+        self.assertFalse(form.is_valid())
