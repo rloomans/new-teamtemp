9da1804af2afd71b87f894ed32bff01c25559070
Robert Loomans <robert@loomans.org>
2022-06-22 13:03:40 +1000

    add test case

--- teamtemp/tests/forms/test_create_survey_form.py
+++ teamtemp/tests/forms/test_create_survey_form.py
@@ -24,6 +24,12 @@ class CreateSurveyFormTestCases(TestCase):
         form = CreateSurveyForm(data=self.form_data)
         self.assertTrue(form.is_valid())
 
+    def test_missing_confirm_password_survey_settings_form(self):
+        self.form_data["new_password"] = "one"
+        self.form_data["confirm_password"] = none
+        form = CreateSurveyForm(data=self.form_data)
+        self.assertFalse(form.is_valid())
+
     def test_password_mismatch_survey_settings_form(self):
         self.form_data["new_password"] = "one"
         self.form_data["confirm_password"] = "two"
