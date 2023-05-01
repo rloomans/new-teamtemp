e8276504dece2279e783f64ee7b3d4edb986942a
Robert Loomans <robert@loomans.org>
2023-05-02 08:58:54 +1000

    use drf-spectacular rather than coreapi for api schema

--- teamtemp/settings/base.py
+++ teamtemp/settings/base.py
@@ -119,6 +119,8 @@ INSTALLED_APPS = (
     'crispy_bootstrap3',
     'cspreports',
     'csvexport',
+    'drf_spectacular',
+    'drf_spectacular_sidecar',
 )
 
 PASSWORD_HASHERS = (
@@ -251,6 +253,10 @@ CSP_FONT_SRC = (
     'fonts.googleapis.com',
     'cdnjs.cloudflare.com',
 )
+CSP_WORKER_SRC = (
+    "'self'",
+    'blob:',
+)
 CSP_EXCLUDE_URL_PREFIXES = ("/djadmin",)
 CSP_REPORT_URI = reverse_lazy('report_csp')
 CSP_REPORTS_FILTER_FUNCTION = "cspreports.filters.filter_browser_extensions"
@@ -262,7 +268,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'PAGE_SIZE': 25,
 }
@@ -276,3 +282,14 @@ WORDCLOUD_WIDTH = 400
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap3"
 
 CRISPY_TEMPLATE_PACK = "bootstrap3"
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Team Temperature API',
+    'DESCRIPTION': 'Team Temperature API',
+    'VERSION': '0.0.1',
+    'SERVE_INCLUDE_SCHEMA': True,
+    'SWAGGER_UI_DIST': 'SIDECAR',
+    'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
+    'REDOC_DIST': 'SIDECAR',
+}
+
